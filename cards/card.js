c8d489969f89ae3a3896a517b1795004fac493fd
vf42h5264g-ops <vf42h5264g@privaterelay.appleid.com>
2026-02-02 21:22:54 +0900

    Update card.js

--- cards/card.js
+++ cards/card.js
@@ -1,4 +1,4 @@
 export const CARDS = [
-  { id: "C001", name: "アーリーリサイタル", rarity: "R",  power: 100, hand: "scissors", art: "img/cards/template.png" },
+  { id: "C001", name: "アーリーリサイタル", rarity: "R",  power: 100, hand: "scissors", art: "img/C001.png" },
   
 ];
